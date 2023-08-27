b59aaf0e7a9355bbe4985e5e58747e07613cc66b
Nadia zerin <nadiajerin10@gmail.com>
2023-08-28 01:49:00 +0600

    simple math

---
+++ simplemath.js
@@ -0,0 +1,7 @@
+var givenmoney = 1000;
+var appleprice = 400;
+var orangeprice = 300;
+var totalprice = appleprice + orangeprice;
+console.log(totalprice);
+moneygiven = givenmoney - totalprice;
+console.log(moneygiven);
