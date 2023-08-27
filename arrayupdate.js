026a5710760fa6734512a3ca959d126090fecd3b
Nadia zerin <nadiajerin10@gmail.com>
2023-08-28 02:31:13 +0600

    push and pop

--- arrayupdate.js
+++ arrayupdate.js
@@ -2,3 +2,7 @@ var fruits = ["Apple", "Banana", "orange"];
 var bananaIndex = fruits.indexOf("Banana");
 fruits[bananaIndex] = "Mango";
 console.log(fruits);
+fruits.pop();
+console.log(fruits);
+fruits.push("watermelon");
+console.log(fruits);
