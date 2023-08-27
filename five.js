1f5b69aa2b1d4d33cb7c7ec681002defecec007f
Nadia zerin <nadiajerin10@gmail.com>
2023-08-27 22:15:56 +0600

    how to find index variable use with for loop

---
+++ five.js
@@ -0,0 +1,5 @@
+var numbers [45,46,47,48,49,50];
+for (var i =0;i<7;i++){
+    var number= numbers[i];
+    console.log(number);
+}
--- for.js
+++ for.js
@@ -1,3 +1,7 @@
 for (var roastgiven = 0; roastgiven < 7; roastgiven++) {
   console.log("roast den plz");
 }
+for loop a Amra i diye likhi... jemon 
+for (i=0;i<7;i++){
+    console .log (i);
+}
